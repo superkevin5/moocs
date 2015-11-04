1750040b53712a6080558bcd06e7adbca27739ca
superkevin5 <hlmloveaustralia@gmail.com>
2015-11-05 00:15:23 +1100

    finish form update

--- app/jsoneditor/jsoneditor.js
+++ app/jsoneditor/jsoneditor.js
@@ -1,9 +1,9 @@
-(function(angular) {
+(function (angular) {
   'use strict';
-  angular.module('jsoneditor', ['ui.bootstrap']).controller('jsoneditor', function($scope) {
-
-    var sourceJson = [
-      {"type": "report",
+  angular.module('jsoneditor', ['ui.bootstrap']).controller('jsoneditor', function ($scope) {
+    $scope.sourceJson = [
+      {
+        "type": "report",
         "order": "2",
         "level": "li",
         "category": "Who are the participants?",
@@ -11,9 +11,15 @@
         "page": "main_participants",
         "tableauView": 1,
         "Link": "https://10ay.online.tableau.com/t/unswmooc/views/Whoaretheparticipants_2/Demographics",
-        "description": {"tag":"div","id":"introText","html":"<h2 style=font-style:italic>Report Categories - Who are the participants?</h2><p>This section shows basic demographic information of the registrants, the data sourced from both coursera demographic survey and course pre-course survey.</p><p>The map gives genders distribution of the registrants who has responded to either of the survey. The icons are scaled for easier comparison. The geographical distribution also indicated where they are mostly from.</p><p>The top 5 countries have the most survey respondents are listed in&nbsp;the box.</p>"}
+        "description": {
+          "tag": "div",
+          "id": "introText",
+          "html": "<h2 style=font-style:italic>Report Categories - Who are the participants?</h2><p>This section shows basic demographic information of the registrants, the data sourced from both coursera demographic survey and course pre-course survey.</p><p>The map gives genders distribution of the registrants who has responded to either of the survey. The icons are scaled for easier comparison. The geographical distribution also indicated where they are mostly from.</p><p>The top 5 countries have the most survey respondents are listed in&nbsp;the box.</p>"
+        }
       },
-      {"type": "report",
+      {
+        "type": "report",
+        "key":":",
         "order": "3",
         "level": "li",
         "category": "What did participants do?",
@@ -21,12 +27,57 @@
         "page": "main_activity",
         "tableauView": 1,
         "Link": "https://10ay.online.tableau.com/t/unswmooc/views/Whatdidpartidantsdo_1/Whatdidparticipantsdo_Overview",
-        "description": {"tag":"div","id":"introText","html":"<h2>Report Categories - What did the participants do?</h2><p><br>This section shows what active registrants have done in the course including&nbsp;Use of Video, Forum, Quiz submission and Tests in the course. Also includes peer assessment performance.&nbsp;</p><p>Blue heatmaps&nbsp;give an overview of participants&#39; activities by week.&nbsp;The color is range from 0% (grey) to 100% (dark Blue).&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p>"}
+        "description": {
+          "tag": "div",
+          "id": "introText",
+          "html": "<h2>Report Categories - What did the participants do?</h2><p><br>This section shows what active registrants have done in the course including&nbsp;Use of Video, Forum, Quiz submission and Tests in the course. Also includes peer assessment performance.&nbsp;</p><p>Blue heatmaps&nbsp;give an overview of participants&#39; activities by week.&nbsp;The color is range from 0% (grey) to 100% (dark Blue).&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p>"
+        }
       }
     ];
 
 
+    $scope.categories = [
+      "Overview",
+      "Home",
+      "Who are the participants?",
+      "What did participants do?",
+      "Assessment",
+      "Research",
+      "Content",
+      "Videos",
+      "Forum",
+      "Social media",
+      "Evaluation",
+      "Activities",
+      "Participants",
+      "Videos",
+      "Content",
+      "Forum",
+      "Evaluation",
+      "Social media",
+      "Research"
+    ];
 
+
+
+    $scope.isObj=function(value){
+
+      return angular.isObject(value);
+    };
+
+
+
+    var getRootNodesScope = function() {
+      return angular.element(document.getElementById("tree-root")).scope();
+    };
+
+    $scope.collapseAll = function() {
+      $scope.isOpen =true;
+
+
+      $scope.hello =true;
+
+    };
     var establishTree = function (row, parent) {
 
 
@@ -72,15 +123,15 @@
         tempNode.items = [];
         establishTree(row, tempNode.items);
         sourceTree.push(tempNode);
-      };
+      }
+      ;
 
 
       $scope.list = sourceTree;
     };
 
 
-
-    convertJsonToTree(sourceJson);
+    convertJsonToTree($scope.sourceJson);
 
 
   });
