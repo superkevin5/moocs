f8e3a9612ba2fb8bfbb82c14cd496a78e68cfed3
superkevin5 <hlmloveaustralia@gmail.com>
2015-12-12 18:59:51 +1100

    add

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,32 +9,29 @@
  * Main module of the application.
  */
 angular
-  .module('mooc', [
-    'ui.router',
-    'ngAnimate'
-  ])
-  .config(function($stateProvider, $urlRouterProvider,$locationProvider) {
+    .module('mooc', [
+        'ui.router',
+        'ngAnimate'
+    ])
+    .config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
 
-    //$urlRouterProvider.when('/dashboard', '/dashboard/overview');
-    //  $locationProvider.html5Mode({
-    //    enabled: true,
-    //    requireBase: false
-    //  });
-      //$locationProvider.hashPrefix('!');
-    $urlRouterProvider.otherwise('/login');
+        //$urlRouterProvider.when('/dashboard', '/dashboard/overview');
+        //$locationProvider.html5Mode({enabled: true, requireBase: true});
+        //$locationProvider.hashPrefix('!');
+        $urlRouterProvider.otherwise('/login');
 
-    $stateProvider
-      .state('base', {
-        abstract: true,
-        url: '',
-        templateUrl: 'views/base.html'
-      })
-        .state('login', {
-          url: '/login',
-          parent: 'base',
-          templateUrl: 'views/login.html'
-          //controller: 'LoginCtrl'
-        });
+        $stateProvider
+            .state('base', {
+                abstract: true,
+                url: '',
+                templateUrl: 'views/base.html'
+            })
+            .state('login', {
+                url: '/login',
+                parent: 'base',
+                templateUrl: 'views/login.html'
+                //controller: 'LoginCtrl'
+            });
         //.state('dashboard', {
         //  url: '/dashboard',
         //  parent: 'base',
@@ -52,4 +49,4 @@ angular
         //    templateUrl: 'views/dashboard/reports.html'
         //  });
 
-  });
+    });
