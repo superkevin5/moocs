0421434330da22ded552c35a731a9be58c8c1998
superkevin5 <hlmloveaustralia@gmail.com>
2016-01-30 22:45:24 +1100

    relocate auto-login

--- app/scripts/directives/autologintableau.js
+++ app/scripts/directives/autologintableau.js
@@ -7,29 +7,18 @@ angular.module('mooc')
     .directive('autoLogin', function () {
         var myDirective = {
             restrict: 'E',
-            template: '<form id="search" name="search" action="https://sso.online.tableau.com/public/prelogin/sendPassword?messageId=" method="post" target="_blank" ng-show="false"> <input name="email" value="unswmooc2@gmail.com"/>'+
+            template: '<form id="search" name="search" action="https://sso.online.tableau.com/public/prelogin/sendPassword?messageId=" method="post" target="formresult" ng-show="false"> <input name="email" value="unswmooc2@gmail.com"/>'+
             '<input name="password" value="n0Password"/>'+
             '<input type="submit" id="loginIntoTableau">'+
             '</form>',
             link: function ($scope, iElm, iAttrs, controller) {
-
-              //var form = iElm[0].querySelector('#search');
-              ////
-              //
-              //form.ajaxForm(function() {
-              //  alert("Thank you for your comment!");
-              //  window.close();
-              //});
-
-              //var closeSelf= function(f){
-              //
-              //  console.log('dddddddd');
-              //
-              //
-              //  window.close();
-              //};
-
+              //var childWindow = window.open('', 'formresult');
+              var windows = window.open("","formresult","width=500,height=300,toolbar=0");
               document.search.submit();
+
+              setTimeout(function(){
+                windows.close();
+              },5000);
 
 
             }
